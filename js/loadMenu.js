f0c20c65bb4cbcb731f7b1a7361f8dd2b12de2d5
martinlex <info@lexelius.se>
2016-06-15 15:44:36 +0200

    Fixes incorrect js path.

--- js/loadMenu.js
+++ js/loadMenu.js
@@ -1,8 +1,8 @@
 var xhr= new XMLHttpRequest();
-xhr.open('GET', 'menu.html', true);
+xhr.open('GET', '../partials/menu.html', true);
 xhr.onreadystatechange= function() {
-    if (this.readyState!==4) return;
-    if (this.status!==200) return; // or whatever error handling you want
-    document.getElementById('#nav').innerHTML= this.responseText;
+    if (this.readyState !==4 ) return;
+    if (this.status !==200 ) return;
+    document.getElementById('#nav').innerHTML = this.responseText;
 };
 xhr.send();

d80d0e005d6d823a69180a655a99b13913b99036
martinlex <info@lexelius.se>
2016-06-15 20:45:32 +0200

    Test.

--- js/loadMenu.js
+++ js/loadMenu.js
@@ -3,7 +3,7 @@ xhr.open('GET', '../partials/menu.html', true);
 xhr.onreadystatechange= function() {
     if (this.readyState !==4 ) return;
     if (this.status !==200 ) return;
-    var nav = document.getElementById('#nav');
+    var nav = document.getElementById('nav');
     nav.innerHTML = this.responseText;
 };
 xhr.send();
